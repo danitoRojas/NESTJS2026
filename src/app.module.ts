54f7cadeebbbd7a877fea0f6f90ecb503e21e147
Daniel Roja <daniel.red.jb@gmail.com>
2026-02-14 08:53:13 -0600

    feat: intalacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmpleadosModule } from './empleados/empleados.module';
 import { TareasModule } from './tareas/tareas.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [EmpleadosModule, TareasModule],
+  imports: [EmpleadosModule, TareasModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
